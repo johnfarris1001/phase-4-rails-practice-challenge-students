c967b03becc1a44e21da3c69f26227dddcd0f0c6
John <johnfarris217@gmail.com>
2023-10-12 22:08:57 -0500

    commit tree d722374ac8fa65bb058163d3d4108dc7a6bb26a4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :instructors, only: [:create, :index, :show, :update, :destroy]
+  resources :students, only: [:create, :index, :show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
